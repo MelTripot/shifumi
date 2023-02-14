848145b7805df40d5f59a81b97081ca7ad6fa017
MelTripot <tripot.melanie@ynov.com>
2023-02-14 10:47:19 +0100

    tout marche il manque que de la cosmetique wola

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import ciseaux from "./assets/ciseaux.png";
 import feuille from "./assets/feuille.png";
@@ -7,80 +7,113 @@ import lezard from "./assets/lezard.png";
 import spock from "./assets/spock.png";
 import "./App.css";
 
+const choices = ["pierre", "ciseaux", "feuille", "lezard", "spock"];
+const getRandomChoice = () => {
+  return choices[Math.floor(Math.random() * 5)];
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [ennemy, setEnnemy] = useState(getRandomChoice());
+  const [played, setPlayed] = useState(null);
 
-  return (
-    <div className="App">
-      <div className="col">
-        <div onClick={play("pierre")}>
-          <img src={pierre} className="logo" alt="React logo" />
-        </div>
-        <div onClick={play("feuille")}>
-          <img src={feuille} className="logo" alt="React logo" />
-        </div>
-        <div onClick={play("ciseaux")}>
-          <img src={ciseaux} className="logo" alt="React logo" />
-        </div>
-        <div onClick={play("lezard")}>
-          <img src={lezard} className="logo" alt="React logo" />
-        </div>
-        <div onClick={play("spock")}>
-          <img src={spock} className="logo" alt="React logo" />
-        </div>
-      </div>
-    </div>
-  );
+  console.log("execution", Date.now);
 
-  function play(played) {
-    const choices = ["pierre", "ciseaux", "feuille", "lezard", "spock"];
-
-    let ennemy = choices[Math.floor(Math.random() * 5)];
+  const result = useMemo(() => {
+    console.log(
+      "vous evez jouer" + played + "votre adversaire a jouer" + ennemy
+    );
     if (played == "pierre") {
       if (ennemy == "ciseaux" || ennemy == "lezard") {
-        // TODO GAGNER
+        // setTimeout(() => {
+        //   setEnnemy(getRandomChoice());
+        //   setPlayed(null);
+        // }, 2000);
+        return "Victoire";
       } else if (ennemy == "feuille" || ennemy == "spock") {
-        //TODO Perdu
+        // setTimeout(() => {
+        //   setEnnemy(getRandomChoice());
+        //   setPlayed(null);
+        // }, 2000);
+        return "Défaite";
       } else {
-        //TODO MATCH NULL
+        // setTimeout(() => {
+        //   setEnnemy(getRandomChoice());
+        //   setPlayed(null);
+        // }, 2000);
+        return "Match null";
       }
     } else if (played === "feuille") {
       if (ennemy === "pierre" || ennemy === "spock") {
-        // TODO GAGNER
+        return "Victoire";
       } else if (ennemy === "ciseaux" || ennemy === "lezard") {
-        //TODO Perdu
+        return "Défaite";
       } else {
-        //TODO MATCH NULL
+        return "Match null";
       }
       // win/loss for ciseaux
     } else if (played === "ciseaux") {
       if (ennemy === "feuille" || ennemy === "lezard") {
-        // TODO GAGNER
+        return "Victoire";
       } else if (ennemy === "pierre" || ennemy === "spock") {
-        //TODO Perdu
+        return "Défaite";
       } else {
-        //TODO MATCH NULL
+        return "Match null";
       }
       // win/loss for lezard
     } else if (played === "lezard") {
       if (ennemy === "spock" || ennemy === "feuille") {
-        // TODO GAGNER
+        return "Victoire";
       } else if (ennemy === "pierre" || ennemy === "ciseaux") {
-        //TODO Perdu
+        return "Défaite";
       } else {
-        //TODO MATCH NULL
+        return "Match null";
       }
       // win/loss for spock
     } else {
       if (ennemy === "ciseaux" || ennemy === "pierre") {
-        // TODO GAGNER
+        return "Victoire";
       } else if (ennemy === "lezard" || ennemy === "feuille") {
-        //TODO Perdu
+        return "Défaite";
       } else {
-        //TODO MATCH NULL
+        return "Match null";
       }
+      return "Victoire";
     }
-  }
+  }, [ennemy, played]);
+
+  const play = useCallback(
+    (value) => {
+      setPlayed(value);
+      setEnnemy(getRandomChoice());
+    },
+    [setPlayed]
+  );
+
+  return (
+    <div className="App">
+      <div className="col">
+        <div onClick={() => play("pierre")}>
+          <img src={pierre} className="logo" alt="React logo" />
+        </div>
+        <div onClick={() => play("feuille")}>
+          <img src={feuille} className="logo" alt="React logo" />
+        </div>
+        <div onClick={() => play("ciseaux")}>
+          <img src={ciseaux} className="logo" alt="React logo" />
+        </div>
+        <div onClick={() => play("lezard")}>
+          <img src={lezard} className="logo" alt="React logo" />
+        </div>
+        <div onClick={() => play("spock")}>
+          <img src={spock} className="logo" alt="React logo" />
+        </div>
+      </div>
+      {/* affichage du jeu de l'adverseraise  */}
+      <div>Ennemy: {ennemy}</div>
+      <div>Played: {played}</div>
+      <div>Resultat : {result}</div>
+    </div>
+  );
 }
 
 export default App;
